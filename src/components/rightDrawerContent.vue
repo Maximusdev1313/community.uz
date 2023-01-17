3a3a209f530ad8a3e3b553057067921e99c052f0
maximusdev1313 <maximusdev1313@gmail.com>
2023-01-17 19:12:33 +0500

    created component for right drawer

---
+++ src/components/rightDrawerContent.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <q-img class="absolute-top" src="https://cdn.quasar.dev/img/material.png" style="height: 150px">
+          <div class="absolute-bottom bg-transparent">
+            <q-avatar size="56px" class="q-mb-sm">
+              <img src="https://cdn.quasar.dev/img/boy-avatar.png">
+            </q-avatar>
+            <div class="text-weight-bold">Razvan Stoenescu</div>
+            <div>@rstoenescu</div>
+          </div>
+        </q-img>
+    </div>
+</template>
