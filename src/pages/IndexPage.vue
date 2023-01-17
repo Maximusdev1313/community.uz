f9046ffd5a965f2cc9d9579b2e9cd267bb0ee6be
maximusdev1313 <maximusdev1313@gmail.com>
2023-01-17 19:10:33 +0500

    added inputs for index page

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,17 +1,19 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page  class="q-page-container">
+    <div class="wrapper">
+      <div class="text-h5 text-weight-bolder">Please write your question</div>
+      <InputForWriteMassage/>
+
+    </div>
   </q-page>
 </template>
 
 <script>
+import InputForWriteMassage from 'src/components/inputForWriteMassage.vue';
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: 'IndexPage'
+    name: "IndexPage",
+    components: { InputForWriteMassage }
 })
 </script>
