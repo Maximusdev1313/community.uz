359f6266fed8df7676e8ff8b425c39a9ff9ded44
maximusdev1313 <maximusdev1313@gmail.com>
2023-01-17 19:13:17 +0500

    deleted margins and added text

--- src/components/leftDrawerContent.vue
+++ src/components/leftDrawerContent.vue
@@ -1,5 +1,8 @@
 <template>
-        <q-scroll-area style="height: calc(100% - 150px); margin-top: 150px; border-right: 1px solid #ddd">
+    <div class="text-h5 q-ml-md q-mt-md">
+        Categories
+      </div>
+        <q-scroll-area style="height: calc(100% - 150px);  border-right: 1px solid #ddd">
           <q-list padding>
             <q-item clickable v-ripple>
               <q-item-section avatar>
@@ -42,13 +45,5 @@
             </q-item>
           </q-list>
         </q-scroll-area>
-        <q-img class="absolute-top" src="https://cdn.quasar.dev/img/material.png" style="height: 150px">
-          <div class="absolute-bottom bg-transparent">
-            <q-avatar size="56px" class="q-mb-sm">
-              <img src="https://cdn.quasar.dev/img/boy-avatar.png">
-            </q-avatar>
-            <div class="text-weight-bold">Razvan Stoenescu</div>
-            <div>@rstoenescu</div>
-          </div>
-        </q-img>
+       
 </template>
