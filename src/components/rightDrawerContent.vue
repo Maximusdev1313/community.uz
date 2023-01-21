d7e16ab5fc28b313a3dbf864dc03d164da6e57d5
maximusdev1313 <maximusdev1313@gmail.com>
2023-01-21 20:23:37 +0500

    imported own ques-lists and formatted

--- src/components/rightDrawerContent.vue
+++ src/components/rightDrawerContent.vue
@@ -1,13 +1,22 @@
 <template>
-    <div>
-        <q-img  src="https://cdn.quasar.dev/img/material.png" style="height: 150px">
-          <div class="absolute-bottom bg-transparent">
-            <q-avatar size="56px" class="q-mb-sm">
-              <img src="https://cdn.quasar.dev/img/boy-avatar.png">
-            </q-avatar>
-            <div class="text-weight-bold">Razvan Stoenescu</div>
-            <div>@rstoenescu</div>
-          </div>
-        </q-img>
-    </div>
+  <div>
+    <q-img src="https://cdn.quasar.dev/img/material.png" style="height: 150px">
+      <div class="absolute-bottom bg-transparent">
+        <q-avatar size="56px" class="q-mb-sm">
+          <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
+        </q-avatar>
+        <div class="text-weight-bold">Razvan Stoenescu</div>
+        <div>@rstoenescu</div>
+      </div>
+    </q-img>
+    <div class="container q-pa-md text-subtitle1">Question List</div>
+    <question-list :height="height" />
+  </div>
 </template>
+<script setup>
+import questionList from "./questionList.vue";
+import { ref } from "vue";
+
+// this variable gives dynamic height style for lists
+const height = ref(430);
+</script>
