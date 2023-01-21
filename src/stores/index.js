e0daafd7157efa7a51297c9aec159ffb97cd6a14
maximusdev1313 <maximusdev1313@gmail.com>
2023-01-21 20:30:01 +0500

    create asyc func s for getting category and quests

--- src/stores/index.js
+++ src/stores/index.js
@@ -1,6 +1,42 @@
 import { store } from 'quasar/wrappers'
-import { createPinia } from 'pinia'
+import { createPinia, defineStore } from 'pinia'
+import axios from 'axios'
 
+export const useApiStore = defineStore('store', {
+  state: () => ({
+    categoryApi: [],
+    questions: []
+  }),
+  actions: {
+    async GetCategoryApi() {
+      try {
+        const Fetch_Api = await axios.get('http://maxmaximusdev.pythonanywhere.com/category/')
+        this.categoryApi = Fetch_Api.data
+        console.log(this.categoryApi);
+      } catch (error) {
+        console.log(error.message);
+      }
+    },
+    async GetQuestion(id) {
+      try {
+        const Fetch_Api = await axios.get(`http://maxmaximusdev.pythonanywhere.com/category/${id}`)
+        const category = Fetch_Api.data
+        console.log('from category', category);
+        this.questions = category.questions
+        console.log('from pinia ', this.questions);
+      } catch (error) {
+        console.log(error.message);
+
+      }
+    }
+  }
+
+
+
+
+
+
+})
 /*
  * If not building with SSR mode, you can
  * directly export the Store instantiation;
