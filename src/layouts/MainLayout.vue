4d7a2b3f7d30f1a3ce8ca9657b5f0ad211929813
maximusdev1313 <maximusdev1313@gmail.com>
2023-01-21 20:25:28 +0500

    deleted lists from drawer and imported inside comp

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,53 +1,63 @@
 <template>
   <q-layout view="lHr LpR fFf">
-
     <q-header reveal elevated class="bg-primary text-white">
       <q-toolbar>
-        <q-btn dense flat round icon="menu" class="lt-md" @click="toggleLeftDrawer" />
+        <q-btn
+          dense
+          flat
+          round
+          icon="menu"
+          class="lt-md"
+          @click="toggleLeftDrawer"
+        />
 
         <q-toolbar-title>
           <q-avatar>
-            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg" />
           </q-avatar>
           Title
         </q-toolbar-title>
 
-        <q-btn dense flat round icon="menu" class="lt-md" @click="toggleRightDrawer" />
+        <q-btn
+          dense
+          flat
+          round
+          icon="menu"
+          class="lt-md"
+          @click="toggleRightDrawer"
+        />
       </q-toolbar>
     </q-header>
 
     <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered>
       <!-- drawer content -->
-      
-      <LeftDrawerContent/>
+
+      <LeftDrawerContent />
     </q-drawer>
 
     <q-drawer show-if-above v-model="rightDrawerOpen" side="right" bordered>
       <!-- drawer content -->
-      <right-drawer-content/>
-      <question-list/>
+      <right-drawer-content />
     </q-drawer>
 
     <q-page-container>
       <router-view />
     </q-page-container>
-
   </q-layout>
 </template>
 
 <script setup>
-import { ref } from 'vue'
-import LeftDrawerContent from 'src/components/leftDrawerContent.vue'
-import RightDrawerContent from 'src/components/rightDrawerContent.vue';
-import questionList from 'src/components/questionList.vue';
+import { ref } from "vue";
+import LeftDrawerContent from "src/components/leftDrawerContent.vue";
+import RightDrawerContent from "src/components/rightDrawerContent.vue";
+import questionList from "src/components/questionList.vue";
 const leftDrawerOpen = ref(false);
-    const rightDrawerOpen = ref(false);
-   const toggleLeftDrawer = ()=>{
-        leftDrawerOpen.value = !leftDrawerOpen.value
-      }
+const rightDrawerOpen = ref(false);
+const toggleLeftDrawer = () => {
+  leftDrawerOpen.value = !leftDrawerOpen.value;
+};
 
-     const toggleRightDrawer = ()=>{
-        rightDrawerOpen.value = !rightDrawerOpen.value
-      }
-
+const toggleRightDrawer = () => {
+  rightDrawerOpen.value = !rightDrawerOpen.value;
+};
 </script>
