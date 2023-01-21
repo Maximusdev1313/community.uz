79c427ebd48da910897c4d85022786ecc7bfadb4
maximusdev1313 <maximusdev1313@gmail.com>
2023-01-21 20:16:40 +0500

    formatted with prettier

--- src/components/inputForWriteMassage.vue
+++ src/components/inputForWriteMassage.vue
@@ -1,37 +1,33 @@
 <template>
-    <div class="container">
-        <div class="text-subtitle1 text-weight-bolder text-grey">Please write your question</div>
-
-        <div class=" ">
-            <div>
-                <q-input label="Your name" class="q-py-sm" />
-                <q-input label="Title for question" class="q-pb-sm" />
-            </div>
-            <div class="input">
-
-                <div class="input__field">
-                    <q-input autogrow label="Question" class="q-py-sm cursor-pointer">
-                        <template v-slot:append>
-                            <q-icon name="cloud_upload" />
-                        </template>
-                    </q-input>
-                </div>
-
-            </div>
-
-            <q-btn class="q-my-md">Ready</q-btn>
-        </div>
-        <q-separator></q-separator>
-
+  <div class="container">
+    <div class="text-subtitle1 text-weight-bolder text-grey">
+      Please write your question
     </div>
+
+    <div class=" ">
+      <div>
+        <q-input label="Your name" class="q-py-sm" />
+        <q-input label="Title for question" />
+      </div>
+      <div class="input">
+        <div class="input__field">
+          <q-input autogrow label="Question" class="cursor-pointer">
+            <template v-slot:append>
+              <q-icon name="cloud_upload" />
+            </template>
+          </q-input>
+        </div>
+      </div>
+
+      <q-btn class="q-my-md">Ready</q-btn>
+    </div>
+    <q-separator></q-separator>
+  </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
-let text = ref('')
+import { ref } from "vue";
+let text = ref("");
 </script>
 
-<style scoped>
-
-
-</style>
+<style scoped></style>
