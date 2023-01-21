a12e11f2299d0648ddac1254c1edddc5dfcf5d0c
maximusdev1313 <maximusdev1313@gmail.com>
2023-01-21 20:22:37 +0500

    changed scrolling styles to dynamic styles

--- src/components/questionList.vue
+++ src/components/questionList.vue
@@ -1,25 +1,55 @@
-<template >
-    <div class="container q-pt-xl">
-        <q-scroll-area>
-        <div >
-            Question List
-        </div>
-        <q-list class="q-mt-sm " v-for="q in 8" :key="q">
-            <q-item>
-                <q-item-section>
-                    <q-item-label>Single line item</q-item-label>
-                    <q-item-label caption lines="2">Secondary line text. Lorem ipsum dolor sit amet, consectetur
-                        adipiscit elit.</q-item-label>
-                </q-item-section>
+<template>
+  <div class="container">
+    <q-scroll-area
+      :style="{ height: height + 'px' }"
+      :bar-style="barStyle"
+      :thumb-style="thumbStyle"
+    >
+      <q-list class="q-mt-sm" v-for="q in 20" :key="q">
+        <q-item clickable>
+          <q-item-section>
+            <q-item-label>THis is title</q-item-label>
+            <q-item-label caption lines="2"
+              >Secondary line text. Lorem ipsum dolor sit amet, consectetur
+              adipiscit elit.</q-item-label
+            >
+          </q-item-section>
 
-                <q-item-section side top>
-                    <q-item-label caption>5 min ago</q-item-label>
-                    <q-icon name="star" color="yellow" />
-                </q-item-section>
-            </q-item>
+          <q-item-section side top>
+            <q-item-label caption>5 min ago</q-item-label>
+            <q-icon name="star" color="yellow" />
+          </q-item-section>
+        </q-item>
 
-            <q-separator spaced inset />
-        </q-list>
+        <q-separator spaced inset />
+      </q-list>
     </q-scroll-area>
-    </div>
+  </div>
 </template>
+<script setup>
+import { ref, toRefs } from "vue";
+
+const props = defineProps({
+  questions: Array,
+  height: Number,
+});
+
+const { questions} = toRefs(props);
+
+console.log(questions);
+const thumbStyle = {
+  right: "4px",
+  borderRadius: "5px",
+  backgroundColor: "#027be3",
+  width: "5px",
+  opacity: 0.75,
+};
+
+const barStyle = {
+  right: "2px",
+  borderRadius: "9px",
+  backgroundColor: "#027be3",
+  width: "9px",
+  opacity: 0.2,
+};
+</script>
