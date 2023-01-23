31da6f50e8e5d54e4079d723722b469f66bf1454
maximusdev1313 <maximusdev1313@gmail.com>
2023-01-23 17:35:23 +0500

    creating discussion page for comments

---
+++ src/pages/Discussion.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        Discussion 
+        {{ store.question.id }}
+    </div>
+</template>
+<script setup>
+import { useRoute } from 'vue-router';
+import { useApiStore } from 'src/stores';
+
+import{onBeforeMount, ref} from 'vue'
+const store = useApiStore()
+const route =  useRoute()
+store.GetCategoryApi()
+
+onBeforeMount(() => {
+  store.GetComments(route.params.id);
+});
+
+</script>
