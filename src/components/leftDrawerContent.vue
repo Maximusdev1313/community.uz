ee5c8e2d85684b1dfc2f81e25bd9493b1abe2fdd
maximusdev1313 <maximusdev1313@gmail.com>
2023-01-26 17:27:49 +0500

    format with prettier

--- src/components/leftDrawerContent.vue
+++ src/components/leftDrawerContent.vue
@@ -1,35 +1,31 @@
 <template>
-    <div class="text-h5 q-ml-md q-mt-md">
-        Categories
-      </div>
-        <q-scroll-area style="height: calc(100% - 150px);  border-right: 1px solid #ddd">
-          <q-list padding>
-            <q-item
-              clickable
-              v-ripple
-              :active="category.label"
-              v-for="category in store.categoryApi"
-              :key="category"
-              :to="'/category/' + category.id"
-              >
-              <q-item-section avatar>
-                <q-icon :name="category.icon" />
-              </q-item-section>
+  <div class="text-h5 q-ml-md q-mt-md">Categories</div>
+  <q-scroll-area
+    style="height: calc(100% - 150px); border-right: 1px solid #ddd"
+  >
+    <q-list padding>
+      <q-item
+        clickable
+        v-ripple
+        :active="category.label"
+        v-for="category in store.categoryApi"
+        :key="category"
+        :to="'/category/' + category.id"
+      >
+        <q-item-section avatar>
+          <q-icon :name="category.icon" />
+        </q-item-section>
 
-              <q-item-section>
-                {{ category.category_name }}
-              </q-item-section>
-            </q-item>
-
-            
-          </q-list>
-        </q-scroll-area>
-       
+        <q-item-section>
+          {{ category.category_name }}
+        </q-item-section>
+      </q-item>
+    </q-list>
+  </q-scroll-area>
 </template>
 
 <script setup>
-import { useApiStore } from 'src/stores';
+import { useApiStore } from "src/stores";
 
-const store = useApiStore()
-
+const store = useApiStore();
 </script>
