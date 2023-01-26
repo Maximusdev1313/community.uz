14249ac05d7f59526facf9c624cabc5b9a13d9ab
maximusdev1313 <maximusdev1313@gmail.com>
2023-01-26 17:34:05 +0500

    create card for displaying sigle question

---
+++ src/components/card.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="">
+        <article >
+            <main class="text-h6" style="font-size: 16px;">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eos reiciendis ea nisi voluptates error esse assumenda unde repudiandae deleniti sint.
+            </main>
+            <p class="text-subtitle1 " style="font-size: 14px;">    
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Est nihil eaque dolor commodi amet dolores explicabo consectetur mollitia debitis nesciunt ut cum dolorem magnam quae fuga error eos natus obcaecati voluptatibus ab, vitae optio voluptates iste laborum? Ipsum eaque dolorum accusantium, aliquam minus suscipit voluptate molestias nobis pariatur voluptatibus numquam eos eius placeat commodi temporibus rem quam porro maiores doloremque recusandae non. Suscipit, dignissimos doloribus. Rem aut consectetur est cum impedit perspiciatis, cupiditate at! Et, excepturi aliquid?
+                <q-space />
+            
+            </p>
+
+            <time class="row justify-end">16-sentabr</time>
+        </article>
+        <q-separator/>
+
+    </div>
+
+
+</template>
