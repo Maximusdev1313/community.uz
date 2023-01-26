2879614671ad55b6fdbc40f7c8bc1af7d6102310
maximusdev1313 <maximusdev1313@gmail.com>
2023-01-26 17:29:38 +0500

    import components

--- src/pages/Discussion.vue
+++ src/pages/Discussion.vue
@@ -1,13 +1,20 @@
 <template>
-    <div>
-        Discussion 
-        {{ store.question.id }}
+    <div class="container">
+      <div class="bg-grey-11">
+        <div class="text-h6">
+          Savol
+
+        </div>
+        <card/>
+      </div>
+      <answers/>
     </div>
 </template>
 <script setup>
 import { useRoute } from 'vue-router';
 import { useApiStore } from 'src/stores';
-
+import card from 'src/components/card.vue'
+import answers from 'src/components/answers.vue';
 import{onBeforeMount, ref} from 'vue'
 const store = useApiStore()
 const route =  useRoute()
