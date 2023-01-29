e99b8409462c5c36adfeec3a0d04edc79df577b8
maximusdev1313 <maximusdev1313@gmail.com>
2023-01-29 21:10:31 +0500

    copy inputs and changed any func.s for comments

---
+++ src/components/inputForComments.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="container">
+    <form class=" ">
+      <div>
+        <q-input label="Your name" class="q-py-sm" v-model="userName" />
+      </div>
+      <div class="q-mt-md">
+        <q-editor v-model="editor" @paste="onPaste" ref="editorRef" />
+      </div>
+      <q-btn class="q-my-md" @click="addQuestion">Ready</q-btn>
+    </form>
+    <q-separator></q-separator>
+  </div>
+</template>
+
+<script setup>
+import { ref, toRefs } from "vue";
+let userName = ref("");
+let title = ref("");
+let editor = ref("");
+let editorRef = ref(null);
+const props = defineProps({
+  id: String,
+});
+
+const { id } = toRefs(props);
+const onPaste = (evt) => {
+  // Let inputs do their thing, so we don't break pasting of links.
+  if (evt.target.nodeName === "INPUT") return;
+  let text, onPasteStripFormattingIEPaste;
+  evt.preventDefault();
+  evt.stopPropagation();
+  if (evt.originalEvent && evt.originalEvent.clipboardData.getData) {
+    text = evt.originalEvent.clipboardData.getData("text/plain");
+    editorRef.value.runCmd("insertText", text);
+  } else if (evt.clipboardData && evt.clipboardData.getData) {
+    text = evt.clipboardData.getData("text/plain");
+    editorRef.value.runCmd("insertText", text);
+  } else if (window.clipboardData && window.clipboardData.getData) {
+    if (!onPasteStripFormattingIEPaste) {
+      onPasteStripFormattingIEPaste = true;
+      editorRef.value.runCmd("ms-pasteTextOnly", text);
+    }
+    onPasteStripFormattingIEPaste = false;
+  }
+};
+const date = new Date().getTime();
+const random = Math.floor(Math.random() * 10000);
+const special_id = random.toString() + date + random.toString();
+
+const addQuestion = async () => {
+  try {
+    await fetch("http://maxmaximusdev.pythonanywhere.com/comment/", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        special_id: special_id,
+        user_name: userName.value,
+        comment: editor.value,
+        answers: id.value,
+      }),
+    });
+    // location.reload()
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+</script>
+
+<style scoped></style>
