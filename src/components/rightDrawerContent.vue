e89fc22556ad45343bce78fef51fe1069a1de6dc
maximusdev1313 <maximusdev1313@gmail.com>
2023-02-02 22:03:15 +0500

    adding lists by activity

--- src/components/rightDrawerContent.vue
+++ src/components/rightDrawerContent.vue
@@ -5,17 +5,29 @@
         <q-avatar size="56px" class="q-mb-sm">
           <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
         </q-avatar>
-        <div class="text-weight-bold">Razvan Stoenescu</div>
-        <div>@rstoenescu</div>
+        <div class="text-weight-bold">{{ store.storageName }}</div>
+        
+        <!-- <div>ID: {{ store.storageId }}</div> -->
       </div>
     </q-img>
-    <div class="container q-pa-md title">Your Questions</div>
-    <question-list :height="height" />
+    <div class="container q-pa-md title">Sizning  savollaringiz</div>
+    <div class="row justify-center align-center">
+      <div v-if="!store.storageId" class="parts">
+        Savollar mavjud emas!
+      </div>
+      <div v-else-if="!store.activities.length" class="parts">
+        Kategoriya bo'yicha mavjud emas..
+      </div>
+    </div>
+    <question-list :height="height" :questions="store.activities" v-if="store.activities"/>
   </div>
 </template>
 <script setup>
+import { useApiStore } from "src/stores";
 import questionList from "./questionList.vue";
-import { ref } from "vue";
+import { ref, onMounted } from "vue";
+const store = useApiStore()
+
 
 // this variable gives dynamic height style for lists
 const height = ref(430);
